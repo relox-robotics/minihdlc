c7c4e765fbba8eb5501126f7827ee0f3173813f7
Isac <isac.tornberg@gmail.com>
2023-03-23 14:15:21 +0100

    Enable compiling with cpp

--- minihdlc.h
+++ minihdlc.h
@@ -1,22 +1,31 @@
-#ifndef minihdlc_h
-#define minihdlc_h
+#ifndef MINIHDLC_H
+#define MINIHDLC_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef void (*sendchar_type)(uint8_t data);
-typedef void (*frame_handler_type)(const uint8_t *frame_buffer,
-		uint16_t frame_length);
+	typedef void (*sendchar_type)(uint8_t data);
+	typedef void (*frame_handler_type)(const uint8_t *frame_buffer,
+									   uint16_t frame_length);
 
-#define MINIHDLC_MAX_FRAME_LENGTH 64
+#define MINIHDLC_MAX_FRAME_LENGTH 128
 
-void minihdlc_init(sendchar_type sendchar_function,
-		frame_handler_type frame_hander_function);
-void minihdlc_char_receiver(uint8_t data);
-void minihdlc_send_frame(const uint8_t *frame_buffer, uint8_t frame_length);
+	void minihdlc_init(sendchar_type sendchar_function,
+					   frame_handler_type frame_hander_function);
+	void minihdlc_char_receiver(uint8_t data);
+	void minihdlc_send_frame(const uint8_t *frame_buffer, uint8_t frame_length);
 
-void minihdlc_send_frame_to_buffer(const uint8_t *frame_buffer, uint8_t frame_length);
-uint8_t *minihdlc_get_buffer();
-uint32_t minihdlc_get_buffer_size();
+	void minihdlc_send_frame_to_buffer(const uint8_t *frame_buffer, uint8_t frame_length);
+	uint8_t *minihdlc_get_buffer();
+	uint32_t minihdlc_get_buffer_size();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
